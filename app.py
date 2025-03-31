6a9c95b96f0bd4d809a941ef50f9f9d267b3b0eb
Himanshu Kale <91282384+himanshukale02@users.noreply.github.com>
2025-03-31 15:07:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -336,4 +336,4 @@ def process_frame(frame, location="Unknown"):
     return detected_info
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, threaded=True)
